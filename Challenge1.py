659ab9b4c103f6f2fa8beb2399e2ca375b415e6f
Jose Pina <pinajoma0@gmail.com>
2020-08-18 15:31:32 -0300

    add challenge:

---
+++ Challenge1.py
@@ -0,0 +1,9 @@
+#1.Donwloads date titanic  kaggle.com/c/titanic
+# 2. Merge three CSVs( gender_submission,test y train)
+
+#Answer this questions 
+#1.How many peaple were going titanic?
+#2.Hoy many men and women survived?
+# Top 10 ages taht more survived and the top 10 age not survived
+# 4 How many positions or titles were going titanic? #Note: used regular expresion
+#  5 How much summary the valor tickets in USD ?
