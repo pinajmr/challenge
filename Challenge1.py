75546be7979c3307659582d5a294cfe24b89cabb
Jose Piña <pinajoma0@gmail.com>
2020-08-18 22:03:32 -0300

    challe2 process

--- Challenge1.py
+++ Challenge1.py
@@ -3,7 +3,7 @@
 
 #Answer this questions 
 #1.How many people were going on the titanic?
-#2.Hoy many men and women survived?
+#2.How many men and women survived?
 # Top 10 ages taht more survived and the top 10 age not survived
 # 4 How many positions or titles were going on the titanic? #Note: used regular expression
 #  5 How much summary the valor tickets in USD ?
@@ -23,6 +23,14 @@ def run():
     people_total = df.sort_values(by= 'PassengerId', ascending = 'False').nunique()
     print(f'How many people were going on the titanic : {people_total[0]} people' )
    
+    #2. How many men and women survived?
+
+    df11 = np.concatenate([df2,df1])
+    print(df11.head())
+    print(df11.tail())
+    # gender_survived = df1.groupby(['Survived'])['Sex']
+    # print(gender_survived.head())
+
 
 if __name__ == '__main__':
     run()
