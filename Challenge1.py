e9a6c3ab6e1d7a51fd8c621d22076177de491fc6
Jose Piña <pinajoma0@gmail.com>
2020-08-18 22:30:32 -0300

    upload answer one

--- Challenge1.py
+++ Challenge1.py
@@ -16,8 +16,12 @@ def run():
     df0 = pd.read_csv('gender_submission.csv')
     df1 = pd.read_csv('test.csv')
     df2 = pd.read_csv('train.csv')
-
-    df = pd.concat([df0,df1,df2], ignore_index='False' )
+    
+    df01 = pd.merge(df0, df1  ,on = 'PassengerId', how='inner')
+    
+    df = pd.concat([df2,df01])
+    df.index = range(df.shape[0])
+    
 
     #1. How many people were going the titanic
     people_total = df.sort_values(by= 'PassengerId', ascending = 'False').nunique()
@@ -25,9 +29,9 @@ def run():
    
     #2. How many men and women survived?
 
-    df11 = np.concatenate([df2,df1])
-    print(df11.head())
-    print(df11.tail())
+    # df11 = np.concatenate([df2,df1])
+    # print(df11.head())
+    # print(df11.tail())
     # gender_survived = df1.groupby(['Survived'])['Sex']
     # print(gender_survived.head())
 
